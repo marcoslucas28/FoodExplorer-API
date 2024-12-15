5ebd72754a7c16ed27f6419b225660e3bb39846a
Marcos Lucas <marcoslucaspiresmoraessantos@gmail.com>
2024-12-15 11:46:35 -0300

    Added user update feature

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,4 +1,4 @@
-const { hash } = require("bcryptjs")
+const { hash, compare } = require("bcryptjs")
 const AppError = require("../utils/AppError")
 
 const sqliteConnection = require("../database/sqlite")
@@ -20,6 +20,46 @@ class UsersController {
         await database.run('INSERT INTO users (name, email, password) VALUES (?, ?, ?)', [name, email, hashedPassword])
 
         return res.status(201).json()
+    }
+
+    async update(req, res){
+        const { name, email, password, oldPassword } = req.body
+        const id = req.user.id
+
+        const database = await sqliteConnection()
+
+        const user = await database.get("SELECT * FROM users WHERE id = (?)", [id])
+
+        if(!user.id){
+            throw new AppError("Usuário não encontrado")
+        }
+
+        const emailIsValid = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+
+        if(emailIsValid && emailIsValid !== user.id){
+            throw new AppError("Este e-mail já está em uso")
+        }
+
+        user.name = name ?? user.name
+        user.email = email ?? user.email
+        
+        if(password && !oldPassword){
+            throw new AppError("Informe sua senha antiga para atualiza-la");
+        }
+
+        if(password && oldPassword){
+            const checkOldPassword = await compare(oldPassword, user.password)
+
+            if(!checkOldPassword){
+                throw new AppError("A senha antiga não confere");
+            }
+
+            user.password = await hash(password, 8)
+        }
+
+        await database.run(`UPDATE users SET name = ?, email = ?, password = ?, updated_at = DATETIME('now') WHERE id = ?`, [user.name, user.email, user.password, id])
+
+        return res.json()
     }
 }
 
